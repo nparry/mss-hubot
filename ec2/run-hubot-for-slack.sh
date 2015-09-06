bf72829cd9909aa1e60890a14752f111ddf1c5c6
Nathan Parry <nparry@gmail.com>
2015-09-06 16:23:53 -0400

    EC2 util: Run private redis for the bot

--- ec2/run-hubot-for-slack.sh
+++ ec2/run-hubot-for-slack.sh
@@ -2,6 +2,13 @@
 SLACK_TOKEN=$1
 WUNDERGROUND_KEY=$2
 
+echo "Starting Redis"
+docker pull redis
+docker run \
+  --detach \
+  --name=mss-hubot-brain \
+  redis
+
 while true; do
   echo "Pulling latest bot image"
   docker pull nparry/mss-hubot
@@ -14,12 +21,13 @@ while true; do
 
   echo "Starting bot"
   docker run \
-  --rm --name="mss-hubot" \
-  --net=host \
-  -e REDISTOGO_URL=redis://localhost:6379 \
-  -e HUBOT_SLACK_TOKEN=$SLACK_TOKEN \
-  -e HUBOT_WUNDERGROUND_API_KEY=$WUNDERGROUND_KEY \
-  nparry/mss-hubot -a slack -d
+    --rm \
+    --name=mss-hubot \
+    --link mss-hubot-brain:brain \
+    -e REDISTOGO_URL=redis://brain:6379 \
+    -e HUBOT_SLACK_TOKEN=$SLACK_TOKEN \
+    -e HUBOT_WUNDERGROUND_API_KEY=$WUNDERGROUND_KEY \
+    nparry/mss-hubot -a slack
 
   echo "Bot has exited"
   sleep 2
